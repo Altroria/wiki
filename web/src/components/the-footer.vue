83e204130916ba1e740367c9280f5bf8b16c707d
Altroria <495648123@qq.com>
2022-10-13 20:57:35 +0800

    4-6 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Ant Design ©2018 Created by Ant UED
+  </a-layout-footer>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
