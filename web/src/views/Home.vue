62a6ddc12a383b0ccaf8bbb54b4f100f64eb01fa
Altroria <495648123@qq.com>
2022-10-13 22:05:08 +0800

    5-2 使用axios调用后端接口/ebook/list，解决跨域问题 使用ref和reactive实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,15 +48,41 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>
+        {{ ebooks }}
+        {{ ebooks1 }}
+      </pre>
+
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, onMounted, ref, reactive,toRef} from 'vue';
+import axios from "axios";
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup(){
+    console.log("setup");
+    const ebooks = ref()
+    const ebooks1 = reactive({book: []})
+
+    onMounted(()=>{
+      console.log("onMounted");
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) =>{
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.book = data.content;
+
+        console.log(response)
+      })
+    })
+    return {
+      ebooks,
+      ebooks1: toRef(ebooks1, "book")
+    }
+  }
+
+
 });
 </script>
