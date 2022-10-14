e0b521d21a763d33e779823743ddadd647e7357e
Altroria <495648123@qq.com>
2022-10-14 17:04:37 +0900

    5-4 列表查询接口支持动态sql 封面小图标调整

--- src/main/java/com/sunh/wiki/service/EbookService.java
+++ src/main/java/com/sunh/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.sunh.wiki.req.EbookReq;
 import com.sunh.wiki.resp.EbookResp;
 import com.sunh.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,7 +21,10 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
+
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (EbookResp ebook : respList) {
