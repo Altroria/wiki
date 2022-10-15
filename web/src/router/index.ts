1ae81a8bfd05e352b4cc2affa6dd8759da1babc9
Altroria <495648123@qq.com>
2022-10-15 20:46:27 +0800

    6-2 增加电子书管理页面 新增页面开发：页面、路由、菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,6 +18,12 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     // 懒加载
     // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+
   }
 ]
 
