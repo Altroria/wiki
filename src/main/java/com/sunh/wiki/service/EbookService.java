79240af0765c5f57fe4e7c3e526e8832b195c5dc
Altroria <495648123@qq.com>
2022-10-17 21:28:36 +0800

    6-5 集成PageHelper插件

--- src/main/java/com/sunh/wiki/service/EbookService.java
+++ src/main/java/com/sunh/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.sunh.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sunh.wiki.domain.Ebook;
 import com.sunh.wiki.domain.EbookExample;
 import com.sunh.wiki.mapper.EbookMapper;
 import com.sunh.wiki.req.EbookReq;
 import com.sunh.wiki.resp.EbookResp;
 import com.sunh.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,10 +19,13 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private  static  final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req){
+        PageHelper.startPage(1,3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())){
@@ -26,6 +33,12 @@ public class EbookService {
         }
 
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebooksList);
+        LOG.info("总行数 {}",pageInfo.getTotal());
+        LOG.info("总页数 {}",pageInfo.getPages());
+
+
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (EbookResp ebook : respList) {
 //            EbookResp ebookResp = new EbookResp();
@@ -34,6 +47,8 @@ public class EbookService {
 //            respList.add(ebookResp);
 //        }
         List<EbookResp> list = CopyUtil.copyList(ebooksList, EbookResp.class);
+
+
         return list;
     }
 }
